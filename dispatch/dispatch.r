aeb0aff0de53fe62312b1188bf299e74a4dd7b3a
jtlandis <jtlandis314@gmail.com>
2022-12-18 10:19:11 -0500

    Added "ANY" lookup as well as the ability to make quick unions.

--- dispatch/dispatch.r
+++ dispatch/dispatch.r
@@ -6,20 +6,20 @@
 #     initialize = function(usage, args)
 #   )
 # )
-# 
-# 
+#
+#
 # qual <- function(...) {
 #   out <- rlang::list2(...)
 #   class(out) <- c("qual", class(out))
 #   out
 # }
-# 
+#
 # field <- function(x = character()) {
 #   stopifnot("field object must be a character"=is.character(x))
 #   class(x) <- c("field", class(x))
 #   x
 # }
-# 
+#
 # new_qual <- function(`_class`) {
 #   force(`_class`)
 #   function(...) {
@@ -28,15 +28,15 @@
 #     out
 #   }
 # }
-# 
+#
 # opt <- new_qual("opt")
-# 
+#
 # req <- new_qual("req")
-# 
+#
 # multi <- new_qual("multi")
-# 
+#
 # exclsv <- new_qual("exclsv")
-# 
+#
 # new_field <- function(`_class`) {
 #   force(`_class`)
 #   function(x=character()) {
@@ -45,12 +45,12 @@
 #     out
 #   }
 # }
-# 
+#
 # cmd <- new_field("cmd")
 # flg <- new_field("flg")
 # arg <- new_field("arg")
 
-# 
+#
 # dispatch <- function() {
 #   #browser()
 #   .call <- sys.call(-1)
@@ -89,7 +89,7 @@
 #   #   } else {
 #   #     xtra_args <- .args[.m==0L]
 #   #   }
-#   #   
+#   #
 #   # } else {
 #   #   .i <- 1:length(sig_args)
 #   #   sig <- .args[1:length(sig_args)]
@@ -132,9 +132,9 @@ dispatch <- function() {
 #   is_super <- vapply(sig_evaled, inherits, what = "mod_dispatch_super", FUN.VALUE = logical(1))
 #   if (any(is_super)) {
 #     sig_evaled[is_super] <- lapply(sig_evaled, `[[`, 1)
-#     sig_class[is_super] <- lapply(sig_evaled, `[[`, 2) 
+#     sig_class[is_super] <- lapply(sig_evaled, `[[`, 2)
 #   }
-# 
+#
 #   list(
 #     sig_evaled,
 #     sig_class
@@ -153,7 +153,7 @@ super <- function(from, to) {
 
 #' @export
 new_generic <- function(name, dispatch_args, fun) {
-  
+
   defenv <- parent.frame()
   dspenv <- defenv[[".__dispatch__."]]
   if (is.null(dspenv)) {
@@ -162,6 +162,16 @@ new_generic <- function(name, dispatch_args, fun) {
   genenv <- dspenv[[name]]
   if (is.null(genenv)) {
     genenv <- dspenv[[name]] <- new.env(parent=emptyenv())
+  }
+  if (is.null(fun)) {
+    args <- c(dispatch_args, "...")
+    args <- stats::setNames(lapply(args, function(i) quote(expr = )), args)
+    defenv$.dispatch <- dispatch
+    f <- function() {}
+    formals(f) <- args
+    body(f) <- quote(.dispatch())
+    environment(f) <- defenv
+    fun <- f
   }
   attr(genenv, ".signature") <- dispatch_args
   attr(fun, ".name") <- name
@@ -174,7 +184,7 @@ method <- function(generic, signature){
   gen_name <- attr(generic, ".name")
   genenv <- environment(generic)[[".__dispatch__."]][[gen_name]]
   disp_args <- attr(genenv, ".signature")
- 
+
   if ((n <- length(disp_args))==1) {
     signature_classes <- list(class(signature))
   } else {
@@ -184,7 +194,7 @@ method <- function(generic, signature){
 }
 
 search_table <- function(table, signature_class, i, depth) {
-  
+
   # search along classes in the
   # signature
   arg_class <- signature_class[[i]]
@@ -201,25 +211,36 @@ search_table <- function(table, signature_class, i, depth) {
     } else {
       #Recalling either returns a method
       # or NULL from an error tryCatch
-      
+
       tbl <- Recall(tbl, signature_class, i + 1L, depth)
       # if it is NULL -
       # no method in next arg for current
-      # class_name. continue loop to next 
+      # class_name. continue loop to next
       # class_name
+      if (!is.null(tbl)) return(tbl)
+    }
+  }
+
+  # Check fallback
+  tbl <- table[["ANY"]]
+  if (!is.null(tbl)) {
+    if ( i == depth) {
+      return(tbl)
+    } else {
+      tbl <- Recall(tbl, signature_class, i + 1L, depth)
       if (!is.null(tbl)) return(tbl)
     }
   }
   #Code reaching this point can only be NULL
   NULL
-  
+
 }
 
 find_method <- function(genenv, disp_args, signature_classes, gen_name) {
   p_tbl <- genenv
-  
+
   # search_ <- function(p_tbl, signature_class, i) {
-    # 
+    #
     # search along classes in the
     # signature
   #   arg_class <- signature_class[[i]]
@@ -244,7 +265,7 @@ find_method <- function(genenv, disp_args, signature_classes, gen_name) {
   #       )
   #       # if it is NULL -
   #       # no method in next arg for current
-  #       # class_name. continue loop to next 
+  #       # class_name. continue loop to next
   #       # class_name
   #       if (!is.null(tbl)) return(tbl)
   #     }
@@ -253,29 +274,90 @@ find_method <- function(genenv, disp_args, signature_classes, gen_name) {
   #   stop(sprintf("Could not find method `%s` for arg `%s` of class <%s>",
   #                gen_name,
   #                disp_args[i], paste0(arg_class, collapse = "/")), call. = F)
-  #   
+  #
   # }
-  
+
   method <- search_table(p_tbl, signature_classes, 1L, length(signature_classes))
-  
+
   if (is.null(method)) {
     stop(sprintf("Could not find method %s(%s)",
-                 gen_name, 
+                 gen_name,
                  paste0("<",vapply(
                    signature_classes, `[[`,
-                   1, FUN.VALUE =  character(1)), 
+                   1, FUN.VALUE =  character(1)),
                    ">", collapse = ", ")), call. = F)
   }
-  
+
   method
 
+}
+
+sanitize <- function(signature) {
+  if (is.list(signature)) {
+    lapply(
+      signature,
+      function(x) {
+        y <- x %in% "numeric"
+        if (any(y)) {
+          x <- as.list(x)
+          x[y] <- list(c("integer","double"))
+          unlist(x)
+        } else {
+          x
+        }
+      }
+    )
+  } else if (is.character(signature)) {
+    y <- signature %in% "numeric"
+    if (any(y)) {
+      x <- as.list(signature)
+      x[y] <- list(c("integer", "double"))
+      x
+    } else {
+      signature
+    }
+  }
+}
+
+expand_signature <- function(signature) {
+  stopifnot('`signature` must be a list or character vector'=!is.object(signature))
+  signature <- sanitize(signature)
+  if (is.list(signature)) {
+    stopifnot("all list elements must be character vectors"=all(vapply(signature, is.character, logical(1))))
+    do.call(expand_combo, signature)
+  } else if(is.character(signature)) {
+    list(signature)
+  }
+}
+
+expand_combo <- function(...) {
+  args <- list(...)
+  # signature length
+  n <- length(args)
+  # length of each char vec
+  nargs <- vapply(args, length, integer(1))
+
+  nout <- prod(nargs)
+
+  out <- lapply(1:nout, function(x, n) character(n), n = n)
+
+  nrep <- nout
+  for (i in seq_len(n)) {
+    vec <- args[[i]]
+    nrep <- nrep/nargs[i]
+    vec_rep <- rep_len(rep(vec, each = nrep), nout)
+    for (j in seq_len(nout)) {
+      out[[j]][i] <- vec_rep[j]
+    }
+  }
+  out
 }
 
 #' @export
 `method<-` <- function(generic, signature, value) {
   gen_name <- attr(generic, ".name")
   defenv <- environment(generic)
-  
+
   dspenv <- defenv[[".__dispatch__."]]
   if (is.null(dspenv)) {
     dspenv <- defenv[[".__dispatch__."]] <- new.env(parent = emptyenv())
@@ -285,51 +367,38 @@ find_method <- function(genenv, disp_args, signature_classes, gen_name) {
     eval(substitute(genenv <- dspenv$.name <- new.env(parent = emptyenv()),
                     list(.name = sym_gen)))
   }
-  
+
   disp_args <- attr(genenv, ".signature")
-  if ((n <- length(disp_args))==1) {
-    signature_class <- class(signature)[[1]]
-  } else {
-    signature_class <- vapply(signature, function(x) class(x)[[1]], character(1))
-  }
-  
-  p_tbl <- genenv
-  
-  for (i in seq_len(n)) {
-    class_name <- signature_class[[i]]
-    if (i != n) {
-      tbl <- p_tbl[[class_name]]
-      if (is.null(tbl)) {
-        p_tbl[[class_name]] <- tbl <- new.env(parent = emptyenv())
+  expanded_class <- expand_signature(signature)
+  n <- length(disp_args)
+  i_seq <- seq_len(n)
+  for (j in seq_along(expanded_class)) {
+    p_tbl <- genenv
+    signature_class <- expanded_class[[j]]
+    for (i in i_seq) {
+      class_name <- signature_class[[i]]
+      if (i != n) {
+        tbl <- p_tbl[[class_name]]
+        if (is.null(tbl)) {
+          p_tbl[[class_name]] <- tbl <- new.env(parent = emptyenv())
+        }
+        p_tbl <- tbl
+      } else {
+        if (!is.null(p_tbl[[class_name]])) {
+          message(sprintf(
+            "Overwritting method %s(%s)",
+            gen_name,
+            paste0("<",signature_class, ">", collapse = ", ")
+          ))
+        }
+        p_tbl[[class_name]] <- value
       }
-      p_tbl <- tbl
-    } else {
-      if (!is.null(p_tbl[[class_name]])) {
-        message(sprintf(
-          "Overwritting method %s(%s)",
-          gen_name,
-          paste0("<",signature_class, ">", collapse = ", ")
-        ))
-      }
-      p_tbl[[class_name]] <- value
     }
   }
-  
+
   invisible(generic)
-  
+
 }
 
-# add <- new_generic("add", c("x","y"), function(x, y, ...) dispatch())
-# method(add, list(1, 2)) <- function(x,y) x + y
-# pudo_num <- function(x) {structure(x, class = c("pudo_num", "numeric"))}
-# method(add, list(1L, 2)) <- function(x, y) x + y
-# method(add, list(pudo_num(1), "4")) <- function(x, y) paste0(x, y)
-# 
-# 
-# 
-# foo <- new_generic("foo", c("x", "y"), function(x, y) dispatch())
-# 
-# 
-# 
-# method(foo, list())
+
 
